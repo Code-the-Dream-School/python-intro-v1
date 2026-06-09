fd09985246332bd25124c9f4cc21be025c796b84
Ricardo Santiz <santizricardo@gmail.com>
2026-06-09 10:57:09 -0700

    Complete books scraper

--- assignment8/get_books.py
+++ assignment8/get_books.py
@@ -8,28 +8,75 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 # Task 3
+
+options = webdriver.ChromeOptions()
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1080")
+
 driver = webdriver.Chrome(
-    service=ChromeService(ChromeDriverManager().install())
+    service=ChromeService(ChromeDriverManager().install()),
+    options=options
 )
+
 
 url = "https://durhamcounty.bibliocommons.com/v2/search?query=learning%20spanish&searchType=smart"
 driver.get(url)
 
 results = []
+results = []
 
-book_entries = driver.find_elements(
+book_cards = driver.find_elements(
     By.CSS_SELECTOR,
-    "li.cp-search-result-item-info"
+    "li"
 )
 
-print(len(book_entries))
-
-for book in book_entries:
-    title = book.find_element(
+for book in book_cards:
+    title_elements = book.find_elements(
         By.CSS_SELECTOR,
         "span.title-content"
-    ).text
+    )
 
-    print(title)
+    if len(title_elements) > 0:
+        title = title_elements[0].text
+
+        author_elements = book.find_elements(
+            By.CSS_SELECTOR,
+            "a.author-link"
+        )
+
+        authors = []
+        for author in author_elements:
+            authors.append(author.text)
+
+        author_text = "; ".join(authors)
+
+        format_elements = book.find_elements(
+            By.CSS_SELECTOR,
+            "span.display-info-primary"
+        )
+
+        format_year = ""
+        if len(format_elements) > 0:
+            format_year = format_elements[0].text
+
+        results.append(
+            {
+                "Title": title,
+                "Author": author_text,
+                "Format-Year": format_year
+            }
+        )
+
+books_df = pd.DataFrame(results)
+
+print(books_df)
+
+books_df.to_csv(
+    "assignment8/get_books.csv",
+    index=False
+)
+
+with open("assignment8/get_books.json", "w") as file:
+    json.dump(results, file, indent=4)
 
 driver.quit()
